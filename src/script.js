4a267fd5a7c834de550e4acc7e9b684a0f1c99f5
Stefano Andriolo <stefano@srand.it>
2021-09-05 17:33:16 +0200

    Add subjects highlight detection
    Add code to detect when the mouse spotlight starts/ends intersecting
    with the subjects.
    
    Add the reference to the GSAP library to add animations in a following
    update.
    
    Minor changes and fixes.

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ import * as THREE from 'three'
 import * as dat from 'dat.gui'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { CustomSceneBuilder } from './customSceneBuilder'
+import { gsap } from 'gsap'
 
 // Scene
 const scene = new THREE.Scene()
@@ -44,6 +45,8 @@ window.addEventListener('mousemove', ev => {
     mousePos.x = ev.clientX / sizes.width * 2 - 1
     mousePos.y = - (ev.clientY / sizes.height) * 2 + 1
 })
+
+const subjectsRaycaster = new THREE.Raycaster()
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
@@ -90,6 +93,10 @@ const controls = new OrbitControls(camera, canvasElement)
 controls.enableDamping = true
 controls.enabled = false
 
+const spotLightDirection = new THREE.Vector3()
+THREE.Object3D.prototype.highlighted = false
+THREE.Object3D.prototype.highlightedBy = null
+
 const tick = () => {
     spotlightRaycaster.setFromCamera(mousePos, camera)
     const intersects = spotlightRaycaster.intersectObject(myScene.basePlaneMesh)
@@ -98,6 +105,41 @@ const tick = () => {
         myScene.secondaryLight.target.position.x = planeTargetPoint.x
         myScene.secondaryLight.target.position.y = planeTargetPoint.y
         myScene.secondaryLight.target.position.z = planeTargetPoint.z
+    }
+    
+    myScene.secondaryLight.shadow.camera.getWorldDirection(spotLightDirection)
+    subjectsRaycaster.set(myScene.secondaryLight.position, spotLightDirection)
+    const spotLightIntersects = subjectsRaycaster.intersectObjects(myScene.sceneSubjects, false)
+
+    // find non-intersected objects and reset their highlighted{By} properties
+    myScene.sceneSubjects.filter(sub =>
+        !spotLightIntersects
+            .map(i => i.object)
+            .find(obj => obj.uuid === sub.uuid))
+        .forEach(notIntersected => {
+            if (notIntersected.highlighted) {
+                // change status only when needed
+                notIntersected.highlighted = false
+                notIntersected.highlightedBy = null
+                notIntersected.material?.color.set(0xff0000)
+    
+                // console.log(`object ${notIntersected.uuid} no longer intersected`)
+            }
+        })
+
+    for (const intersection of spotLightIntersects) {
+        if (intersection.object.highlighted) {
+            // already highlighted, do nothing for now
+        } else {
+            // not yet highlighted
+            intersection.object.highlighted = true
+            intersection.object.highlightedBy = myScene.secondaryLight
+
+            // the intersected object actually should be a Mesh
+            intersection.object.material?.color.set(0x00ff00)
+
+            // console.log(intersection)
+        }
     }
 
     controls.update()
